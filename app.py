ffd34f74780506eddc444e6c076ae0538a82dff5
time-time-time <68034191+time-time-time@users.noreply.github.com>
2025-05-10 23:55:05 -0400

    new port

--- app.py
+++ app.py
@@ -153,6 +153,6 @@ def analyze():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 6000))
+    port = int(os.environ.get("PORT", 3000))
     print(f"Flask development server starting on host 0.0.0.0, port {port}")
     app.run(host='0.0.0.0', port=port, debug=False)
